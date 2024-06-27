c691c568bff51bda77a4b02ba3165763ae2d294e
Nick Bossi <nickbossiwork@gmail.com>
2024-06-27 16:59:38 +0200

    K-fold working

--- sinwave_4_k-folds.py
+++ sinwave_4_k-folds.py
@@ -10,35 +10,10 @@ from torchvision import datasets, transforms
 import torch.optim as optim
 import time
 from torch.utils.data import Dataset, DataLoader
+import copy
+import numpy as np
 
 from sklearn.model_selection import KFold
-
-# Global Parameters
-batch_size = 4
-
-# Dimensions of data
-input_size = 100
-num_samples = 400
-
-
-# Setting up the Dataset object
-class SinDataset(Dataset):
-    def __init__(self, data, transform = None):
-        self.data = torch.tensor(data, dtype = torch.float32)
-        self.transform = transform 
-
-
-    def __len__(self):
-        return len(self.data)
-    
-    def __getitem__(self, idx):
-        sample = self.data[idx]
-
-        #applies transformation when item is called
-        if self.transform:
-            sample = self.transform(sample)
-        
-        return sample
 
 # Setting the device to cuda 
 device = (
@@ -50,9 +25,18 @@ device = (
 )
 print(f"Using {device} device")
 
+# Hypeparameters
+batch_size = 4
+learning_rate = 0.01
+l2 = 0.01
+l2_list = np.linspace(0,0.5,20)
+
+# Dimensions of data
+input_size = 100
+num_samples = 400
 
 numbers = sorted([random.uniform(0,4*math.pi) for x in range(input_size)])
-random_sample = torch.tensor([math.sin(number) for  number in numbers]).to(device) 
+random_sample = torch.tensor([math.sin(number) for  number in numbers])
 # print(random_sample.shape)
 
 #random_samples = sorted([random.uniform(0, 4*math.pi) for x in range(input_size)])
@@ -68,7 +52,7 @@ class CustomDataset(Dataset):
         self.output_tensor = output_tensor
         self.mean = self.input_tensor.mean(dim = 0, keepdim = True)
         self.std = self.input_tensor.std(dim = 0, keepdim = True)
-        #self.transform = transform
+        self.transform = transform
 
     def __len__(self):
         return(len(self.input_tensor))
@@ -86,23 +70,20 @@ class CustomDataset(Dataset):
         return x,y
     
 
-#plt.plot(input[0,:].detach().numpy(), response[0,:].detach().numpy())
-#plt.show()
-
 # Normalization transformation
 normalize = transforms.Normalize(torch.mean(input), torch.std(input))
 
 data = CustomDataset(input, response)
 dataload = DataLoader(data, batch_size=batch_size)
 
-# Testing dataloader
-for batch_idx, batch in enumerate(dataload):
-    print(batch_idx)
-    inputs, targets = batch
-    print("Input tensor: ")
-    print(inputs)
-    print("Output tensor: ")
-    print(targets)
+# # Testing dataloader
+# for batch_idx, batch in enumerate(dataload):
+#     print(batch_idx)
+#     inputs, targets = batch
+#     print("Input tensor: ")
+#     print(inputs.shape)
+#     print("Output tensor: ")
+#     print(targets.shape)
     
 # Defining our neural net
 class Net(nn.Module):
@@ -128,77 +109,33 @@ class Net(nn.Module):
         out = self.linear_relu_stack(input)
         return out
 
-net = Net().cuda()
-criterion = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr = 0.1)
-
-def train_one_epoch(epoch_index):
-    running_loss = 0 
-    last_loss = 0
-    for i, data in enumerate(dataload):
-
-        input, response = data
-        input = input.to(device)
-        response = response.to(device)
-
-        optimizer.zero_grad()
-
-        output = net(input).squeeze(1)
-
-        loss = criterion(output, response)
-        loss.backward()
-
-        optimizer.step()
-
-        running_loss += loss.item()
-
-        if i % 4 == 3:                 #batch size is 4
-            last_loss = running_loss/4 #loss per batch
-            print(' batch {} loss {}'.format(i+1, last_loss))
-            running_loss = 0
-        print('epoch {}: loss {}'.format(epoch_index+1, last_loss))
-    
-    # Gives loss of training
-    return last_loss
-
-
-for epoch in range(10):
-    print('EPOCH {}'.format(epoch+1))
-
-    #ensures gradient tracking is on
-    net.train(True)
-    avg_loss = train_one_epoch(epoch)   #gets loss associated with training epoch
-
-'''
-pred_input=input[0,:].to(device)
-pred_input = (pred_input- torch.mean(pred_input))/torch.std(pred_input)
-predictions = net(pred_input).cpu()
-pred_input=pred_input.cpu().detach().numpy()
-plt.plot(pred_input, response[0,:].detach().numpy(), label = "actual", color = "blue", linestyle="-")
-plt.plot(pred_input, predictions.detach().numpy(), label = "predicted", color = "red", linestyle="-")
-plt.show()
-'''
-
-'''
-# Loss Function
 loss_fn = nn.MSELoss()
 
-# Defining our training function
+# Defining out training function
 def train(model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, data in enumerate(train_loader):
+    model.train(True)
+    train_loss = 0.0
+    for i, data in enumerate(train_loader):
         inputs, targets = data
         inputs = inputs.to(device)
         targets = targets.to(device)
+
         optimizer.zero_grad()
-        output = model(inputs)
+
+        output = model(inputs).squeeze(1)
+
         loss = loss_fn(output, targets)
         loss.backward()
         optimizer.step()
 
-# K fold cross validation
+        train_loss += loss              #accumulates total loss
+    train_loss = train_loss/(i+1)       #gets average loss over batches
+    return train_loss                   #returns average loss
 
-EPOCHS = 10
+######################################## K fold cross validation ##################################################
+
+#sets number of epoch and best_v_loss which will be used to track which model gives best validation performance
+EPOCHS = 8
 best_v_loss = 1000000
 epoch_number = 0
 
@@ -206,178 +143,67 @@ k_folds = 9
 
 kf = KFold(n_splits=k_folds, shuffle = True)
 
-print(kf.split(data))
+#Setting model and optimizer
+model = Net().to(device)
 
-for fold, (train_idx,test_idx) in enumerate(kf.split(data)):
-    print("Fold {}".format(fold))
-    print("-------------------")
+for i in l2_list:
 
-    #Defining dataloaders for current fold
+    optimizer = optim.Adam(model.parameters(), lr = learning_rate, weight_decay=i)
 
-    train_load = DataLoader(dataset=data,
-                            batch_size=batch_size,
-                            sampler = torch.utils.data.SubsetRandomSampler(test_idx))
-    
-    valid_load = DataLoader(dataset= data, 
-                            batch_size = batch_size,
-                            sampler = torch.utils.data.SubsetRandomSampler(test_idx))
-    
-    model = Net().to(device)
-    optimizer = optim.Adam(model.parameters(), lr = 0.01, weight_decay=0.01)
+    for fold, (train_idx,test_idx) in enumerate(kf.split(data)):
+        print("++++++++++++++++++")
+        print("Fold {}".format(fold+1))
+        print("++++++++++++++++++")
 
-    for epoch in range(EPOCHS):
-        train(model, device, dataload, optimizer, epoch)
+        #Defining dataloaders for current fold
 
-'''
-'''
-input_data = torch.sin(numbers1)
-print(input_data.shape)
-print(input_data[:,0])
-#input_data = torch.tensor([([math.sin(number) for number in sorted([random.uniform(0, 4*math.pi) for x in range(input_size)])]) for i in range(num_samples)])
-
-# Splitting into Training, Testing and Validation Sets
-train_data = input_data[:18].to(device)
-#validation_data = input_data[16:18].to(device)
-test_data = input_data[18:].to(device)
-
-
-
-# Creating datasets for each
-train_set = SinDataset(train_data, transform=normalize)
-#validation_set = SinDataset(validation_data, transform = normalize)
-test_set = SinDataset(test_data, transform = normalize)
-
-# Creating DataLoaders for each
-train_loader = DataLoader(train_data,  batch_size = 4, shuffle = True)
-#validation_loader = DataLoader(validation_data, batch_size = 2, shuffle = True)
-test_loader = DataLoader(test_data, batch_size = 2, shuffle = True)
-
-print('Training set has {} instances'.format(len(train_set)))
-#print('Validation set has {} instances'.format(len(validation_set)))
-
-#input = torch.tensor([math.sin(number) for number in random_samples])
-input_data = input_data.to(device)
-
-#print(input_data[:,0])
-
-dataset = SinDataset(input_data, transform=normalize)
-
-dataloader = DataLoader(dataset, batch_size=10, shuffle = True)
-
-
-
-# Initialises an instance of our neural net and ensuring it's on GPU
-net = Net().cuda()
-#print(net)
-
-# gets the number of matrices of weights and biases,
-# 6 in this case (3x weights, 3x biases) for the three layers
-params = list(net.parameters())
-
-#print(len(params))
-#extracts the dimensions of the first hidden layer's weights matrix
-#print(params[0].size())
-
-#setting loss function and optimizer
-loss_fn = nn.MSELoss()
-optimizer = optim.SGD(net.parameters(), lr = 0.1)
-'''
-
-
-
-
-
-'''
-# Defining the training over an epoch
-def train_one_epoch(epoch_index):
-    for i, data in enumerate(train_loader):
-
-        running_loss = 0 
-        last_loss = 0
-
-        input = data
-
-        optimizer.zero_grad()
-
-        output = net(input)
-
-        loss = criterion(output, input)
-        loss.backward()
-
-        optimizer.step()
-
-        running_loss += loss.item()
-
-        if i % 4 == 3:                 #batch size is 4
-            last_loss = running_loss/4 #loss per batch
-            print(' batch {} loss {}'.format(i+1, last_loss))
-            running_loss = 0
-        print('epoch {}: loss {}'.format(epoch_index+1, last_loss))
-    
-    # Gives loss of training
-    return last_loss
-
-#timestamp = time.datetime.now().strftime('%Y%m%d_%H%M%S')
-'''
-
-'''
-
-
+        train_load = DataLoader(dataset=data,
+                                batch_size=batch_size,
+                                sampler = torch.utils.data.SubsetRandomSampler(test_idx))
         
+        valid_load = DataLoader(dataset= data, 
+                                batch_size = batch_size,
+                                sampler = torch.utils.data.SubsetRandomSampler(test_idx))
+        
+        # Training all epoch for each fold
 
-'''
+        for epoch in range(EPOCHS):
+            print('EPOCH {}'.format(epoch+1))
+            print("-------------------")
+            
+            avg_t_loss = train(model, device, train_load, optimizer, epoch)   #trains for an epoch and then outputs average loss over epoch
 
+            #Turns on model evaluation, more efficient for validation as not training
+            running_vloss = 0.0
+            model.eval()
 
+            # Disables gradient tracking, reduces memory consumption
+            with torch.no_grad():
+                for i, vdata in enumerate(valid_load):
+                    vinputs, vtargets = vdata
+                    vinputs = vinputs.to(device)
+                    vtargets = vtargets.to(device)
+                    vouptuts = model(vinputs).squeeze(1)
+                    vloss = loss_fn(vouptuts, vtargets)
+                    running_vloss +=vloss
+            
+            avg_v_loss = running_vloss/(i+1)            #calculates average validation loss
+            print('LOSS train {} valid {}'.format(avg_t_loss, avg_v_loss))            #outputs training vs validation loss
 
-'''
+            # Checks to see if validation loss has improved and saves the model as the optimal model
+            if avg_v_loss < best_v_loss:
+                best_v_loss = avg_v_loss
+                optimal_model = copy.deepcopy(model)
+                #model_path = 'model_{}'.format(epoch_number+1)
+                #torch.save(model.state_dict(), model_path)
 
-for epoch in range(EPOCHS):
-    print('EPOCH {}'.format(epoch_number+1))
+pred_input = torch.tensor(numbers)
+pred_input = (pred_input - torch.mean(pred_input))/torch.std(pred_input)
+prediction = optimal_model(torch.tensor(pred_input).to(device))
 
-    #ensures gradient tracking is on
-    net.train(True)
-    avg_loss = train_one_epoch(epoch_number)   #gets loss associated with training epoch
+plt.plot(numbers, random_sample, label = 'input', color = 'blue', linestyle = "-")
 
-    #Turns on model evaluation, more efficient for validation as not training
-    running_vloss = 0.0
-    net.eval()
-
-    # Disables gradient tracking, reduces memory consumption
-    with torch.no_grad():
-        for i, vdata in enumerate(validation_loader):
-            vouptuts = net(vdata)
-            vloss = criterion(vouptuts, vdata)
-            running_vloss +=vloss
-    
-    avg_vloss = running_vloss/(i+1)
-    print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
-
-    if avg_vloss < best_v_loss:
-        best_v_loss = avg_vloss
-        model_path = 'model_{}'.format(epoch_number+1)
-        torch.save(net.state_dict(), model_path)
-    
-    epoch_number +=1
-
-
-#Recovering optimal model 
-saved_model = Net()
-print(model_path)
-saved_model.load_state_dict(torch.load(model_path))
-
-#Making final prediction and plotting
-final_pred = saved_model(random_sample.cpu())
-
-drawinput = (torch.sin(numbers1[19,:])).numpy()
-drawoutput = (final_pred.cpu()).detach().numpy()
-print(drawinput)
-print(drawoutput)
-
-
-plt.plot(numbers1[19,:].numpy(), drawinput, label = 'input', color = 'blue', linestyle = "-")
-
-plt.plot(numbers1[19,:].numpy(), drawoutput, label = 'output', color = 'red', linestyle = "-")
+plt.plot(numbers, prediction.cpu().detach().numpy(), label = 'output', color = 'red', linestyle = "-")
 plt.grid(True)
 plt.show()
 
-'''
